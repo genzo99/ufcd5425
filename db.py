d49cce65976d663ebb8551947b5d0fd1c619d37c
genzo99 <carlosmart99@gmail.com>
2026-09-03 11:35:16 +0100

    Update db.py

--- db.py
+++ db.py
@@ -4,17 +4,14 @@ DB_NAME = "task_manager.db"
 
 
 def get_connection():
-    """Cria e devolve uma ligação à base de dados SQLite."""
     conn = sqlite3.connect(DB_NAME)
     return conn
 
 
 def init_db():
-    """Cria as tabelas do projeto, caso ainda não existam."""
     conn = get_connection()
     cursor = conn.cursor()
 
-    # Tabela de projetos
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS projetos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -24,7 +21,6 @@ def init_db():
         );
     """)
 
-    # Tabela de tarefas
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS tarefas (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -95,6 +91,63 @@ def listar_tarefas(id_projeto):
         FROM tarefas
         WHERE id_projeto = ?
     """, (id_projeto,))
+
+    tarefas = cursor.fetchall()
+    conn.close()
+    return tarefas
+
+
+def editar_tarefa(id_tarefa, novo_titulo, nova_prioridade, novo_estado):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE tarefas
+        SET titulo = ?, prioridade = ?, estado = ?
+        WHERE id = ?
+    """, (novo_titulo, nova_prioridade, novo_estado, id_tarefa))
+
+    conn.commit()
+    conn.close()
+
+
+def remover_tarefa(id_tarefa):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM tarefas WHERE id = ?", (id_tarefa,))
+    conn.commit()
+    conn.close()
+
+
+# ---------------------------
+# FILTROS
+# ---------------------------
+
+def filtrar_tarefas_por_estado(id_projeto, estado):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT id, titulo, prioridade, estado, data_criacao
+        FROM tarefas
+        WHERE id_projeto = ? AND estado = ?
+    """, (id_projeto, estado))
+
+    tarefas = cursor.fetchall()
+    conn.close()
+    return tarefas
+
+
+def filtrar_tarefas_por_prioridade(id_projeto, prioridade):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT id, titulo, prioridade, estado, data_criacao
+        FROM tarefas
+        WHERE id_projeto = ? AND prioridade = ?
+    """, (id_projeto, prioridade))
 
     tarefas = cursor.fetchall()
     conn.close()
